28343026e6c72187fe0c1424deb0c07ad6c1a345
bmnazmussakib <nazmussakib161@gmail.com>
2021-07-02 18:10:33 +0600

    function

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//Function 1
+function greet(){
+    document.getElementById('greetings'). innerText='SSB';
+};
+
+//Function 2
+function greet2(){
+    document.getElementById('greet2'). innerText='This is Javascript';
+};
+
+//Function 3
+function dblclick(){
+    document.getElementById('three'). innerText='You have double clicked';
+};
+
+//Function 4
+function alertclick(){
+    alert('You have clicked alert !!!!!');
+};
+
+//Function 5
+function large(){
+    document.getElementById('large').style.fontSize = '50px';
+};
+
+//Function 6
+function color(){
+    document.getElementById('color').style.color = 'blue';
+};
